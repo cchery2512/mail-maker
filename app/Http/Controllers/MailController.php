d857f6f5e3a1e727748ec8f236940ad8532abd9b
Carmelo Chery <carmelo.chery@procuraduria.gob.pa>
2021-07-15 10:04:25 -0500

    FINAL CONFIGURE

--- app/Http/Controllers/MailController.php
+++ app/Http/Controllers/MailController.php
@@ -3,17 +3,17 @@
 namespace App\Http\Controllers;
 use App\Mail\TestMail;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Mail;
+use Mail;
 
 class MailController extends Controller
 {
     public function sendEmail(){
         $details = [
             'title'     => 'Mail from CCHERY',
-            'body'      => 'This is for testing mail using gmail.' 
+            'body'      => 'Este es un correo de Prueba enviado desde el backend.' 
         ];
 
-        Mail::to('cchery1225@gmail.com')->send(new TestMail($details));
+        Mail::to('cchery2512@gmail.com')->send(new TestMail($details));
         return "Email Sent";
     }
 }
